187ef53d4c42087c8b46d7b838292a683ff8c9d2
Harshit Parmar <parmarharshit2402@gmail.com>
2025-06-24 10:33:00 +0530

    Making Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import { assets } from "../assets/assets";
+import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
 
 
 
@@ -11,15 +12,23 @@ const Navbar = () => {
                 <img src={assets.logo} alt="" className="w-36 h-auto"/>
             </link>
 
-            <div>
+            <div className=" max-md:absolute max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300">
+                <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer"/>
+
+                <Link to='/' >Home</Link>
+                <Link to='/movies' >Movies</Link>
+                <Link to='/' >Theaters</Link>
+                <Link to='/' >Releases</Link>
+                <Link to='/favorite' >Favorites</Link>
 
             </div>
 
-            <div>
-
+            <div className="flex items-center gap-8">
+                <SearchIcon className="'max-md:hidden w-6 h-6 cursor-pointer" />
+                <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer" >Login</button>
             </div>
 
-            <MenuIcon className = ''
+            <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" />
         </div>
     )
 }
