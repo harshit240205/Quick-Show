9ac539d6e361ca80993e447151f3290079930ae3
Harshit Parmar <parmarharshit2402@gmail.com>
2025-06-24 10:10:10 +0530

    Making of Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import { assets } from "../assets/assets";
+
+
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
+            <link to = '/' className="max-md:flx-1">
+                <img src={assets.logo} alt="" className="w-36 h-auto"/>
+            </link>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+            <MenuIcon className = ''
         </div>
     )
 }
